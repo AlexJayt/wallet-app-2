a43e217debb7f587d4ec060cda981e7a2a72f92e
Alex <yeromenk@gmail.com>
2017-04-09 19:47:59 +0300

    Redirect other path to home page

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'login', loadChildren: 'app/log-in/log-in.module#LogInModule'},
-  { path: '', loadChildren: 'app/home/home.module#HomeModule'}
+  { path: 'login', loadChildren: 'app/log-in/log-in.module#LogInModule' },
+  { path: '', loadChildren: 'app/home/home.module#HomeModule' },
+  { path: '**', redirectTo: '' }
 ]
